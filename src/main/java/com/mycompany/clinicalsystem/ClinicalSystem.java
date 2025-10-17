c1d11d3e84ad03ca39b1e1afa66dfa269346ff2d
danielSanchez <daniel.sanchez14@udea.edu.co>
2025-10-17 11:11:30 -0500

    Primer commit
    Inicialización del proyecto y repositorio.

---
+++ src/main/java/com/mycompany/clinicalsystem/ClinicalSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicalsystem;
+
+/**
+ *
+ * @author kosmo
+ */
+public class ClinicalSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
