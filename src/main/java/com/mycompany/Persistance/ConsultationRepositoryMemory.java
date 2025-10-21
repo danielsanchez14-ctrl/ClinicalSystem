316cf658ed2f9f7f6c0dca14ea5d08c0bffdac78
danielSanchez <daniel.sanchez14@udea.edu.co>
2025-10-21 18:03:46 -0500

    Implement in-memory ConsultationRepository and update main
    Added a .gitignore to exclude the /target/ directory. Implemented the ConsultationRepositoryMemory class with in-memory storage and methods for adding and searching consultations.

--- src/main/java/com/mycompany/Persistance/ConsultationRepositoryMemory.java
+++ src/main/java/com/mycompany/Persistance/ConsultationRepositoryMemory.java
@@ -6,7 +6,9 @@ package com.mycompany.Persistance;
 
 import com.mycompany.Interfaces.IConsultationRepository;
 import com.mycompany.Models.Consultation;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -14,26 +16,43 @@ import java.util.List;
  */
 public class ConsultationRepositoryMemory implements IConsultationRepository {
 
-    private List<Consultation> repo;
+    private final List<Consultation> repo;
+
+    public ConsultationRepositoryMemory() {
+        this.repo = new ArrayList<>();
+    }
+
+    public ConsultationRepositoryMemory(List<Consultation> repo) {
+        this.repo = repo;
+    }
 
     @Override
     public boolean add(Consultation consultation) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (repo != null) {
+            this.repo.add(consultation);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public List<Consultation> searchByPatient(String patientID) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.repo.stream().
+                filter(c -> c.getAppointment().getPatient().getId().equals(patientID))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Consultation> searchByDoctor(String doctorID) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.repo.stream().
+                filter(c -> c.getAppointment().getDoctor().getId().equals(doctorID))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Consultation> listAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.repo;
     }
 
 }
